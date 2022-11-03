4077103034aaf095e481d7bc05d90a5a9ae0ef7c
bilroum <bilroum@gmail.com>
2022-11-03 21:56:04 +0100

    added component to display online users

---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,25 @@
+import { useCollection } from "../hooks/useCollection";
+//styles
+import "./OnlineUsers.css";
+
+import React from "react";
+import Avatar from "./Avatar";
+
+export default function OnlineUsers() {
+  const { documents, error } = useCollection("users");
+  return (
+    <div className="user-list">
+      <h2>All Users</h2>
+      {error && <div className="error">{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div key={user.id} className="user-list-item">
+            {user.online && <span className="online-user"></span>}
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+}
